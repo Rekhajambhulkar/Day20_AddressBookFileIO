eefbd012b367ddabccf518c564b056fc79206b57
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-12-11 11:53:10 +0530

    Welcome Message

---
+++ AddressBookMainClass.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMainClass {
+		public static void main(String[] args) {
+					System.out.println("\n\tWelcome to Address Book Program");
+		}
+}
